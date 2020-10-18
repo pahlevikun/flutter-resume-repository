21452fd61a901a4e21a9d8881526c386866be3dc
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2020-10-19 05:23:19 +0700

    Add downloadable content

--- lib/presentation/landing/section/header_section.dart
+++ lib/presentation/landing/section/header_section.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pahlevikun.github.io/common/config/app_config.dart';
 import 'package:pahlevikun.github.io/common/config/screen_util.dart';
 import 'package:pahlevikun.github.io/data/resume/resume_data.dart';
+import 'dart:js' as js;
 
 import '../../base_page.dart';
 
@@ -79,6 +80,32 @@ class HeaderSection extends StatelessWidget {
                           SizedBox(width: SizeConfig.SMALL_SIZE),
                           Text(
                             "Contact Me",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    MaterialButton(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: SizeConfig.LARGE_SMALL_SIZE,
+                          vertical: SizeConfig.SMALL_SIZE),
+                      onPressed: (){
+                        js.context.callMethod('open', ['/files/CV_20201019.pdf']);
+                      },
+                      color: AppConfig.secondaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.file_download,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: SizeConfig.SMALL_SIZE),
+                          Text(
+                            "Download Resume",
                             style: TextStyle(color: Colors.white),
                           )
                         ],
